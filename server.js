1bcf2b44da693a0438f4259d70730687cd9c98d2
kennethgodwin06 <kennethgodwin06@gmail.com>
2016-09-21 14:12:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,28 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+        'article-One':{
+            title: 'Article One| Kenneth Godwin',
+            heading:'Article One',
+            date:'Sepetember 21 ,2016',
+            content:`<p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article. 
+                    </p>
+                    <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                    <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p> `
+         },
+        'article-two':{
+            title: 'Article Two| Kenneth Godwin',
+            heading:'Article Two',
+            date:'Sepetember 21 ,2016',
+            content:`<p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article. 
+                    </p>
+                    <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                    <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p> `
+        },
+        'article-three':{
+            title: 'Article Three| Kenneth Godwin',
+            heading:'Article Three',
+            date:'Sepetember 21 ,2016',
+            content:`<p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article. 
+                    </p>
+                    <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                    <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p> `
+        }
+    };
 
-var articleOne= {
-    title: 'Article One| Kenneth Godwin',
-    heading:'Article One',
-    date:'Sepetember 21 ,2016',
-    content:`<p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article. 
-            </p>
-            <p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>
-            <p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first articl.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p> `
-    
-};
+
+
 
 function createTemplate(data){
-    var title=data.title
-    var heading=data.heading
-    var date=data.date
-    var content=data.content
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
     
     
     var htmlTemplate=`<html>
@@ -64,16 +95,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function (req,res){
-   res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));   
-});
-
-app.get('/article-three',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));  
+app.get('/:articleName',function (req,res){
+    var articleName=req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
