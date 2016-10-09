1006f8105af608dad708277225dcb2df250e49bb
kennethgodwin06 <kennethgodwin06@gmail.com>
2016-10-09 10:41:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
-console.log('Loaded!');
+//counter code
+var couner=0;
+var button=document.getElementById('counter');
+button.onclick=function (){
+  
+  
+  
+  
+  
+  counter=counter+1;
+  var span=document.getElementById('count');
+  span.innerHtml=counter.toString();
+    
+};
